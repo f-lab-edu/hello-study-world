28086016b02e5641c96f85408ce8880b2ac248df
holee <ho.sol.lee@gmail.com>
2022-10-16 22:59:48 +0900

    feat: CreateUserUseCase의 랜덤 User 객체 생성 시 fake value 누락 부분 수정

--- app-core/src/test/kotlin/test/domain/user/UserTestUtils.kt
+++ app-core/src/test/kotlin/test/domain/user/UserTestUtils.kt
@@ -4,15 +4,19 @@
  */
 package test.domain.user
 
+import com.flab.hsw.core.domain.user.User
 import com.flab.hsw.core.domain.user.usecase.CreateUserUseCase
 import com.flab.hsw.core.domain.user.usecase.EditUserUseCase
 import com.github.javafaker.Faker
+import com.github.javafaker.service.FakeValuesService
+import com.github.javafaker.service.RandomService
+import java.util.*
 
 fun randomCreateUserMessage(
     nickname: String = Faker().name().fullName(),
     email: String = Faker().internet().emailAddress(),
-    loginId: String = "",
-    password: String = ""
+    loginId: String = FakeValuesService(Locale.ENGLISH, RandomService()).regexify(User.LOGIN_ID_REGEX),
+    password: String = FakeValuesService(Locale.ENGLISH, RandomService()).regexify(User.PASSWORD_REGEX),
 ): CreateUserUseCase.CreateUserMessage {
     data class FakeCreateUserMessage(
         override val nickname: String,
