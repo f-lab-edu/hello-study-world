f6db4d48b7dc74e206138afe380d577e0c451ab2
holee <ho.sol.lee@gmail.com>
2022-10-28 01:38:03 +0900

    feat: PasswordEncryptor 패스워드 일치 여부 검증 메서드 추가 (#10)
    BCrypt는 동일 plain text에 반환되는 salt값이 상이합니다. 따라서 CreateUserUseCase.userIsCreated() TC, 로그인 시 패스워드 검증이 가능하도록 메서드를 새로 추가했습니다.

--- app-core/src/main/kotlin/com/flab/hsw/core/domain/user/aggregate/PasswordEncryptor.kt
+++ app-core/src/main/kotlin/com/flab/hsw/core/domain/user/aggregate/PasswordEncryptor.kt
@@ -6,6 +6,8 @@ interface PasswordEncryptor {
 
     fun encrypt(plainText: String): String
 
+    fun isMatched(plainText: String, hashedPassword: String): Boolean
+
     companion object {
         fun newInstance(): PasswordEncryptor = PasswordEncryptorImplByBCrypt()
     }
@@ -15,5 +17,9 @@ internal class PasswordEncryptorImplByBCrypt : PasswordEncryptor {
 
     override fun encrypt(plainText: String): String {
         return BCrypt.hashpw(plainText, BCrypt.gensalt());
+    }
+
+    override fun isMatched(plainText: String, hashedPassword: String): Boolean {
+        return BCrypt.checkpw(plainText, hashedPassword);
     }
 }
