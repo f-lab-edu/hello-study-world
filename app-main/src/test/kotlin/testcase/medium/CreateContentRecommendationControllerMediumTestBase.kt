87e5a1282356c2cc773b6e9ee623d9bc18b06e46
holee <ho.sol.lee@gmail.com>
2022-12-18 18:07:36 +0900

    test: 컨텐츠 추천 MediumTestBase 설정 수정 (#32)
    '컨텐츠 추천' 기능 테스트에 필요한 객체들을 추가합니다.

--- app-main/src/test/kotlin/testcase/medium/CreateContentRecommendationControllerMediumTestBase.kt
+++ app-main/src/test/kotlin/testcase/medium/CreateContentRecommendationControllerMediumTestBase.kt
@@ -5,22 +5,33 @@
 package testcase.medium
 
 import com.flab.hsw.core.domain.content.usecase.CreateContentRecommendationUseCase
-import com.flab.hsw.core.domain.user.User
+import com.flab.hsw.core.domain.user.repository.UserRepository
+import com.flab.hsw.core.domain.user.usecase.CreateUserUseCase
+import com.flab.hsw.core.domain.user.usecase.UserLoginUseCase
+import com.flab.hsw.endpoint.auth.AuthenticationControllerImpl
 import com.flab.hsw.endpoint.health.HealthControllerImpl
 import com.flab.hsw.endpoint.v1.content.recommend.CreateContentRecommendationControllerImpl
+import com.flab.hsw.endpoint.v1.user.create.CreateUserControllerImpl
+import com.flab.hsw.endpoint.v1.user.login.UserLoginControllerImpl
+import com.flab.hsw.util.JwtTokenManager
+import org.mockito.Mock
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.mock.web.MockHttpSession
 import org.springframework.test.context.ContextConfiguration
-import test.domain.user.aggregate.randomUser
 
 @ContextConfiguration(
     classes = [
         HealthControllerImpl::class,
-        CreateContentRecommendationControllerImpl::class
+        CreateContentRecommendationControllerImpl::class,
     ]
 )
 class CreateContentRecommendationControllerMediumTestBase : MockMvcMediumTestBase() {
 
     @MockBean
     protected lateinit var createContentRecommendationUseCase: CreateContentRecommendationUseCase
+
+    @MockBean
+    protected lateinit var userRepository: UserRepository
+
+    @MockBean
+    protected lateinit var jwtTokenManager: JwtTokenManager
 }
