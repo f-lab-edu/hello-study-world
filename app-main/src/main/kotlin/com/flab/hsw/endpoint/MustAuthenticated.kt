4abff4f3da54bc3d689baf195c9a3b43ccedf738
holee <ho.sol.lee@gmail.com>
2022-12-24 02:15:30 +0900

    docs: @MustAuthenticated 어노테이션의 목적과 동작 과정을 설명하는 주석 추가 (#31)

--- app-main/src/main/kotlin/com/flab/hsw/endpoint/MustAuthenticated.kt
+++ app-main/src/main/kotlin/com/flab/hsw/endpoint/MustAuthenticated.kt
@@ -2,7 +2,11 @@ package com.flab.hsw.endpoint
 
 
 /**
- * <p>이 어노테이션이 작성된 핸들러 메서드는 클라이언트의 요청을 처리할 때 반드시 인증을 필요로 합니다.</p>
+ * 어노테이션이 작성된 핸들러 메서드는 클라이언트의 요청을 처리할 때 반드시 인증을 필요로 합니다.
+ *
+ * Spring의 InterceptorRegistry에 등록된 [com.flab.hsw.interceptor.AuthenticationInterceptor]를 통해 인증 절차를 수행합니다.
+ *
+ * @see [com.flab.hsw.interceptor.AuthenticationInterceptor]
  */
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
