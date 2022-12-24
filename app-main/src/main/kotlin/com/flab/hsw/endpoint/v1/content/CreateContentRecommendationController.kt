50caff72f7585c9e760b09d31a89b397d9e59f9b
holee <ho.sol.lee@gmail.com>
2022-12-24 12:01:25 +0900

    feat: 신규 정의한 CustomArgumentResolver를 사용하도록 로직 수정 (#31)
    @AuthenticatedUser 어노테이션과 AuthenticatedUserArgumentResolver를 사용해 파라미터로 loginId를 전달받도록 수정합니다.
    
    See also: ed79081, 7cc958e

--- app-main/src/main/kotlin/com/flab/hsw/endpoint/v1/content/CreateContentRecommendationController.kt
+++ app-main/src/main/kotlin/com/flab/hsw/endpoint/v1/content/CreateContentRecommendationController.kt
@@ -2,6 +2,7 @@ package com.flab.hsw.endpoint.v1.content
 
 import com.flab.hsw.core.domain.content.usecase.CreateContentRecommendationUseCase
 import com.flab.hsw.core.domain.user.repository.UserRepository
+import com.flab.hsw.endpoint.AuthenticatedUser
 import com.flab.hsw.endpoint.MustAuthenticated
 import com.flab.hsw.endpoint.v1.ApiPathsV1
 import com.flab.hsw.endpoint.v1.content.common.ContentRecommendationResponse
@@ -10,8 +11,6 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpSession
 
 /**
  * ```
@@ -35,7 +34,7 @@ interface CreateContentRecommendationController {
         method = [RequestMethod.POST]
     )
     fun createContentRecommendation(
-        servletRequest: HttpServletRequest,
+        @AuthenticatedUser recommenderLoginId: String,
         @RequestBody request: CreateContentRecommendationRequest
     ): ContentRecommendationResponse
 }
--- app-main/src/main/kotlin/com/flab/hsw/endpoint/v1/content/recommend/CreateContentRecommendationControllerImpl.kt
+++ app-main/src/main/kotlin/com/flab/hsw/endpoint/v1/content/recommend/CreateContentRecommendationControllerImpl.kt
@@ -2,13 +2,11 @@ package com.flab.hsw.endpoint.v1.content.recommend
 
 import com.flab.hsw.core.domain.content.usecase.CreateContentRecommendationUseCase
 import com.flab.hsw.core.domain.content.usecase.CreateContentRecommendationUseCase.CreateContentRecommendationMessage
-import com.flab.hsw.core.domain.user.User
 import com.flab.hsw.core.domain.user.exception.UserByLoginIDNotFoundException
 import com.flab.hsw.core.domain.user.repository.UserRepository
 import com.flab.hsw.endpoint.v1.content.CreateContentRecommendationController
 import com.flab.hsw.endpoint.v1.content.common.ContentRecommendationResponse
 import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 internal class CreateContentRecommendationControllerImpl(
@@ -17,17 +15,15 @@ internal class CreateContentRecommendationControllerImpl(
 ) : CreateContentRecommendationController {
 
     override fun createContentRecommendation(
-        servletRequest: HttpServletRequest,
-        request: CreateContentRecommendationRequest,
+        recommenderLoginId: String,
+        request: CreateContentRecommendationRequest
     ): ContentRecommendationResponse {
-        val authorizedLoginId = servletRequest.getAttribute(User.AUTHORIZED_USER_ID_ALIAS) as String
-
         return ContentRecommendationResponse.from(
             createRecommendUseCase.createContentRecommendation(
                 CreateContentRecommendationMessage(
                     recommendedContentId = request.contentId,
                     recommenderId = userRepository.findByLoginId(
-                        authorizedLoginId)?.id ?: throw UserByLoginIDNotFoundException(authorizedLoginId)
+                        recommenderLoginId)?.id ?: throw UserByLoginIDNotFoundException(recommenderLoginId)
                 )
             )
         )
