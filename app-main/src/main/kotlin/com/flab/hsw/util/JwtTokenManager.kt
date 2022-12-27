6ec3bbc96b8ed5c72561f73295cf4273b5bea2fd
holee <ho.sol.lee@gmail.com>
2022-12-28 00:31:33 +0900

    feat: 공개키를 반환하는 토큰 관리자 기능 및 핸들러 삭제 (#30)

--- app-main/src/main/kotlin/com/flab/hsw/util/JwtTokenManager.kt
+++ app-main/src/main/kotlin/com/flab/hsw/util/JwtTokenManager.kt
@@ -40,14 +40,11 @@ class JwtTokenManager(
             .parseClaimsJws(token.removePrefix(BEARER_PREFIX))
     }
 
-    fun returnEncodedPublicKey(): String = encoder.encodeToString(publicKey.encoded)
-
     companion object {
 
         const val AUTHORIZATION_HEADER = "Authorization"
         const val BEARER_PREFIX = "Bearer "
 
         private val expireTime: Instant = Instant.now().plusSeconds(7200L)
-        private val encoder: Base64.Encoder = Base64.getEncoder()
     }
 }
