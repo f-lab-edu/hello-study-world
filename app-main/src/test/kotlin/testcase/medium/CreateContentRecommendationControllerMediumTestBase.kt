58f0f9c3aed2935800ef8659486a4c602f9c05d5
holee <ho.sol.lee@gmail.com>
2022-12-31 21:00:12 +0900

    feat: 사용되지 않는 의존성 제거 (#32)

--- app-main/src/test/kotlin/testcase/medium/CreateContentRecommendationControllerMediumTestBase.kt
+++ app-main/src/test/kotlin/testcase/medium/CreateContentRecommendationControllerMediumTestBase.kt
@@ -6,15 +6,9 @@ package testcase.medium
 
 import com.flab.hsw.core.domain.content.usecase.CreateContentRecommendationUseCase
 import com.flab.hsw.core.domain.user.repository.UserRepository
-import com.flab.hsw.core.domain.user.usecase.CreateUserUseCase
-import com.flab.hsw.core.domain.user.usecase.UserLoginUseCase
-import com.flab.hsw.endpoint.auth.AuthenticationControllerImpl
 import com.flab.hsw.endpoint.health.HealthControllerImpl
 import com.flab.hsw.endpoint.v1.content.recommend.CreateContentRecommendationControllerImpl
-import com.flab.hsw.endpoint.v1.user.create.CreateUserControllerImpl
-import com.flab.hsw.endpoint.v1.user.login.UserLoginControllerImpl
 import com.flab.hsw.util.JwtTokenManager
-import org.mockito.Mock
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
 
